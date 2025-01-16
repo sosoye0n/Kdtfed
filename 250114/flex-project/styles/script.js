9d185597a7ac64a6e816dbb3467ad1cea5acb319
sosoyeon6176 <sosoyeon98@naver.com>
2025-01-16 16:12:49 +0900

    250116-2

--- 250114/flex-project/styles/script.js
+++ 250114/flex-project/styles/script.js
@@ -17,8 +17,10 @@
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
     document.querySelector("header").classList.add("active");
+    document.querySelector(".gototop").classList.add("active");
   } else {
     document.querySelector("header").classList.remove("active");
+    document.querySelector(".gototop").classList.remove("active");
   }
 });
 
@@ -27,8 +29,59 @@ $(document).ready(function () {
   $(".myslider").slick({
     slidesToShow: 3,
     slidesToScroll: 3,
-    isFinite: false,
+    iFinite: false,
     dots: true,
     speed: 300,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          speed: 300,
+          dots: true,
+          isFinite: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          speed: 300,
+          dots: true,
+          iFinite: false,
+        },
+      },
+    ],
+  });
+});
+
+/* JS에게 이벤트 대상이 누구인지 알려주기 */
+
+/* 이벤트 대상이 되어지는 요소(*html에서 찾아온 태그 / 클래스 / 아이디선택자)에게 어떤 이벤트를 부여할 것인가 결정 */
+
+/* 이벤트를 첫 번째 적용할 때, 두 번째 적용할 때를 상이하게 작동하도록 제어 */
+
+/* trigger Event */
+const trigger = document.querySelector(".trigger");
+
+trigger.addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".gnb").classList.toggle("active");
+});
+
+/* menu Event */
+const menus = document.querySelectorAll(".gnb a .gototop");
+menus.forEach((menu) => {
+  menu.addEventListener("click", function (event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    const targetPosition = targetElement.offsetTop;
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth",
+    });
   });
 });
