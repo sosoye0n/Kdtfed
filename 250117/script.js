8d558d40e78f9ff2fe1c4cd4804a205c8c90f7d3
sosoyeon6176 <sosoyeon98@naver.com>
2025-01-17 17:14:04 +0900

    250117

---
+++ 250117/script.js
@@ -0,0 +1,59 @@
+// 변수 = variable
+// 변수 = 변할 수 있는 값 = 수
+// 변수 = 바구니 / 값을 담을 수 있는 그릇
+
+// 선언 & 할당 => 단계
+
+// 선언하는 방식 => var(*예약어) (2015 이전까지) - 실무에서 사용 x
+// 선언하는 방식 => let || const 추가
+
+// 모든 변수, 저마다 고유한 이름 => 변수명 || 식별자
+
+//변수선언(const) > 변수명,식별자(num) > 변수 값 할당-연산자 사용(=1) >
+// const num = 1;
+
+// // consol(*전쟁에서 메신저를 주고 받는 장치)
+// console.log(num);
+
+// const = constant = 상수(*고정값) <-> 변수
+// 재선언 && 재할당 x
+
+// 변수를 선언하는 순간 => 컴퓨터는 데이터(*변수값) => 특정 메모리 영역 할애 => 메모리가 담길 수 있는 방 생성 => 해당 방에 이름은 식별자
+
+// const num = 2;
+// num = 2;
+
+// 프로그래밍 언어는 엄격할 수록 안정성이 뛰어나다 => 위험요소 감소
+
+// let 재선언 불가 / 재할당은 간으//
+// let str = "Hello World";
+
+// console.log(str);
+
+// // let str = "Me too";
+
+// str = "Me too";
+
+// console.log(str);
+
+// // var 실무에서 사용하지 않음
+
+// var bool = true;
+
+// console.log(bool);
+
+// var bool = false;
+
+// console.log(bool);
+
+// bool = "Restart";
+
+// console.log(bool);
+
+// var num01 = 1;
+
+// var result = num01 + num02;
+
+// var num02 = 2;
+
+// console.log(result);
