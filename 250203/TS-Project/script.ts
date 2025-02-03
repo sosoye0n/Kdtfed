7d5aba613ce6cde570f86dfccdfc0d0a2985aec7
sosoyeon6176 <sosoyeon98@naver.com>
2025-02-03 18:09:19 +0900

    250203-2

---
+++ 250203/TS-Project/script.ts
@@ -0,0 +1,8 @@
+// type notation = 타입 주석
+// 꼭 타입 주석을 입력하지 않더라도 기본적으로 TS는 타입 추론을 할 수 있음!
+let num: number = 1;
+let str = "Hello";
+let bool = true;
+
+bool = false;
+bool = "close";
--- 250203/todolist/scirpt.js
+++ 250203/todolist/scirpt.js
@@ -1,6 +1,4 @@
-/* Event Trigger : 등록(*submit) 버튼 > 클릭시
-
-*/
+/* Event Trigger : 등록(*submit) 버튼 > 클릭시 */
 
 // const accept = document.querySelector("input[type='submit']");
 
@@ -15,24 +13,33 @@
 const form = document.querySelector("form");
 const userInput = document.querySelector("#todo-item");
 const ul = document.querySelector("ul");
+
 let todos = [];
+
+const save = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
 const delItem = (event) => {
   const target = event.target.parentElement;
+  todos = todos.filter((todo) => todo.id != target.id);
+  // 위 두개 타입이 달라 깊은 비교 > 오류 // 의도적으로 얕은 비교
+  save();
   target.remove();
 };
 
-const addItem = (text) => {
-  if (text !== "") {
+const addItem = (todo) => {
+  if (todo.text !== "") {
     const li = document.createElement("li");
     const span = document.createElement("span");
     const button = document.createElement("button");
 
-    span.innerText = text;
+    span.innerText = todo.text;
     button.innerText = "삭제";
     button.addEventListener("click", delItem);
 
     li.append(span, button);
+    li.id = todo.id;
     ul.prepend(li);
   }
 };
@@ -40,16 +47,28 @@ const addItem = (text) => {
 const handler = (event) => {
   event.preventDefault();
 
-  const todo = userInput.value;
+  const todo = {
+    id: Date.now(),
+    text: userInput.value,
+  };
   todos.push(todo);
 
   addItem(todo);
+  save();
   userInput.value = "";
 };
 
-form.addEventListener("submit", handler);
+const init = () => {
+  const userTodos = JSON.parse(localStorage.getItem("todos"));
+  if (userTodos) {
+    userTodos.forEach((todo) => {
+      addItem(todo);
+    });
+    todos = userTodos;
+  }
+};
 
-console.log(todos);
+form.addEventListener("submit", handler);
 
 // 삭제 fuction >27번행
 // span태그 전체가 삭제 되어야 > 자신의 부모인 li가 삭제되어야
